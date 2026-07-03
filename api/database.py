0d3d61f6e26fa8033bbe2ae8b6465a65da9f4c34
Pralisha Tripathy <pralishatripathy000@gmail.com>
2026-07-04 01:36:51 +0530

    Use Render PostgreSQL database URL

--- api/database.py
+++ api/database.py
@@ -1,14 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from urllib.parse import quote_plus
+import os
 
-DB_USER = "postgres"
-DB_PASSWORD = quote_plus("pralisha")
-DB_HOST = "127.0.0.1"
-DB_PORT = "5432"
-DB_NAME = "AuditIQ_DB"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+if not DATABASE_URL:
+    DB_USER = "postgres"
+    DB_PASSWORD = quote_plus("pralisha")
+    DB_HOST = "127.0.0.1"
+    DB_PORT = "5432"
+    DB_NAME = "AuditIQ_DB"
+
+    DATABASE_URL = f"postgresql://auditiq_user:XLvKtT8sEgfOrdq4QmZGOo21sQXzlp1Z@dpg-d9417lsvikkc73badla0-a/auditiq"
 
 engine = create_engine(DATABASE_URL)
 
